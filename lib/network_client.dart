64aa2330cca77044af21de75dff4d1d6c027adc8
France! <franciscodomingosjose06798@gmail.com>
2024-06-01 13:44:50 +0100

    finesh dio baseOptions config

--- lib/network_client.dart
+++ lib/network_client.dart
@@ -2,10 +2,20 @@
 import 'package:dio/dio.dart';
 
 String get _baseUrl{
- return "";
+ return "https://routex-demo.onrender.com";
 }
 
 Dio _createDio(){
-  var dio = Dio() ;
+  var dio = Dio(
+    BaseOptions(
+      baseUrl: _baseUrl,
+      receiveTimeout: const Duration(seconds: 30),
+      connectTimeout: const Duration(seconds: 30),
+      sendTimeout: const Duration(seconds: 60),
+      headers: {},
+    )
+  ) ;
+
+  // dio.interceptors.add();
   return dio;
 }
