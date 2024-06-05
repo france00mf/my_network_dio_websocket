eb440d0feeac0de8cf99c79b4254ebf63ebb1ecc
France! <franciscodomingosjose06798@gmail.com>
2024-06-05 17:56:30 +0100

    add river pod pack#, add provider class

--- lib/network_client.dart
+++ lib/network_client.dart
@@ -2,6 +2,7 @@
 import 'dart:io';
 
 import 'package:dio/dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:my_network_dio_websocket/errors/failure.dart';
 import 'package:my_network_dio_websocket/network_intercepetors.dart';
 
@@ -154,4 +155,114 @@ Future<dynamic> sendFormData(FormDataType requestType,{
       rethrow;
     }
   }
+
+
+  Future<T> patch<T>(
+    
+    String uri, {
+    Map<String, dynamic> queryParameters = const {},
+    Object body = const {},
+    CancelToken? cancelToken,
+    ProgressCallback? onReceiveProgress,
+    Map<String, dynamic>? requestHeaders,
+  }) async {
+    try {
+      Response response = await _dio.patch(
+        uri,
+        queryParameters: queryParameters,
+        data: body,
+        cancelToken: cancelToken,
+        onReceiveProgress: onReceiveProgress,
+        options: Options(
+          headers: requestHeaders ?? _headers,
+        ),
+      );
+      return response.data;
+    } on Failure {
+      rethrow;
+    }
+  }
+
+    Future<T> delete<T>(
+    String uri, {
+    Map<String, dynamic> queryParameters = const {},
+    Map<String, dynamic>? requestHeaders,
+    CancelToken? cancelToken,
+  }) async {
+    try {
+      Response response = await _dio.delete(
+        uri,
+        queryParameters: queryParameters,
+        cancelToken: cancelToken,
+        options: Options(
+          headers: requestHeaders ?? _headers,
+        ),
+      );
+      return response.data;
+    } on Failure {
+      rethrow;
+    }
+  }
+
+
+   Future<dynamic> sendArrayImagesFormData(
+    FormDataType requestType, {
+    required String uri,
+    Map<String, dynamic> queryParameters = const {},
+
+
+    List<File> images = const [],
+    CancelToken? cancelToken,
+    ProgressCallback? onReceiveProgress,
+  }) async {
+    try {
+      var tempList = [];
+      for (var i = 0; i < images.length; i++) {
+        var item = images[i];
+        var multipartFile = await MultipartFile.fromFile(
+          item.path,
+          
+        );
+        tempList.add(multipartFile);
+      }
+
+      FormData formData = FormData.fromMap(
+        {
+          'images': tempList,
+        },
+      );
+      Response response;
+      if (FormDataType.patch == requestType) {
+        response = await _dio.patch(
+          uri,
+          queryParameters: queryParameters,
+          data: formData,
+          cancelToken: cancelToken,
+          onReceiveProgress: onReceiveProgress,
+          options: Options(
+            headers: _headers,
+          ),
+        );
+      } else {
+        response = await _dio.post(
+          uri,
+          queryParameters: queryParameters,
+          data: formData,
+          cancelToken: cancelToken,
+          onReceiveProgress: onReceiveProgress,
+          options: Options(headers: _headers),
+        );
+      }
+      return response.data;
+    } on Failure {
+      rethrow;
+    }
+  }
+
+  final networkClientProvider = Provider<NetWorkClient>(
+    (ref) => NetWorkClient(),
+  );
+
+
+
 
