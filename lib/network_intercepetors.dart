536ca3f2aa6a2bb7ef16f9f514b1727b99ef5aa4
France! <franciscodomingosjose06798@gmail.com>
2024-06-05 18:07:20 +0100

    add app logger util

--- lib/network_intercepetors.dart
+++ lib/network_intercepetors.dart
@@ -1,6 +1,22 @@
 
 import 'package:dio/dio.dart';
+import 'package:my_network_dio_websocket/utils/app_logger.dart';
 
 class NetworkServiceInterceptors extends Interceptor{
-  
+  final _logger = const AppLogger(NetworkServiceInterceptors);
+  final Dio dio;
+  String? token;
+  final String errorKey;
+  Function(DioException err, ErrorInterceptorHandler handler)? onErrorCustom;
+
+  NetworkServiceInterceptors(
+    this.dio,
+
+    {
+      this.errorKey='',
+    this.onErrorCustom
+    }
+  );
+
+
 }
