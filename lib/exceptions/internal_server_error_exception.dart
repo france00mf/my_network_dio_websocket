b454f263d561890515f305b93f0d2aad5cad9947
France! <franciscodomingosjose06798@gmail.com>
2024-06-18 10:11:15 +0100

    finesh internar exception

--- lib/exceptions/internal_server_error_exception.dart
+++ lib/exceptions/internal_server_error_exception.dart
@@ -1,23 +1,32 @@
 import 'package:dio/dio.dart';
 import 'package:my_network_dio_websocket/errors/errors.dart';
 import 'package:my_network_dio_websocket/exceptions/get_error.dart';
+import 'package:my_network_dio_websocket/utils/app_logger.dart';
 
 class InternalServerErrorException extends DioException implements Failure{
+
+  final _logger = appLogger(InternalServerErrorException);
+  final RequestOptions oRequest;
+  final Response? iResponse;
+
+  InternalServerErrorException(
+    this.oRequest, [this.iResponse]
+  ): super(requestOptions: oRequest, response: iResponse);
+
   @override
   // TODO: implement title
-  String get title => throw UnimplementedError();
+  String get title => "Erro de servidor";
 
   @override
   String toString(){
     return "Erro em :\n Titulo $title \n Mensagem $message";
   }
 
-  @override
-  String get title => "Erro de servidro";
+
 
   @override
   String get message {
-    _logger.e(serverResponse?.data);
+    _logger.e(iResponse?.data);
     return "Erro desconhecido, tente novamente";
   }
 
