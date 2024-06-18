cba3c34e413566b34a3fd654b9bbf3b59973b26e
France! <franciscodomingosjose06798@gmail.com>
2024-06-18 10:03:23 +0100

    implements on internal servers expcetion

--- lib/exceptions/internal_server_error_exception.dart
+++ lib/exceptions/internal_server_error_exception.dart
@@ -1,1 +1,27 @@
-class InternalServerErrorException{}
+import 'package:dio/dio.dart';
+import 'package:my_network_dio_websocket/errors/errors.dart';
+import 'package:my_network_dio_websocket/exceptions/get_error.dart';
+
+class InternalServerErrorException extends DioException implements Failure{
+  @override
+  // TODO: implement title
+  String get title => throw UnimplementedError();
+
+  @override
+  String toString(){
+    return "Erro em :\n Titulo $title \n Mensagem $message";
+  }
+
+  @override
+  String get title => "Erro de servidro";
+
+  @override
+  String get message {
+    _logger.e(serverResponse?.data);
+    return "Erro desconhecido, tente novamente";
+  }
+
+  String getErrorInfo(error, [String key='']){
+      return getErrorInfoFromResponse(error);
+  }
+}
