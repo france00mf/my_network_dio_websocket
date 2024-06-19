e531a087ae091bea7bd5a7735a057571d8ccb798
France! <franciscodomingosjose06798@gmail.com>
2024-06-19 15:41:32 +0100

    finesh conflictException

--- lib/exceptions/conflict_exception.dart
+++ lib/exceptions/conflict_exception.dart
@@ -2,6 +2,7 @@
 
 import 'package:dio/dio.dart';
 import 'package:my_network_dio_websocket/errors/errors.dart';
+import 'package:my_network_dio_websocket/exceptions/get_error.dart';
 import 'package:my_network_dio_websocket/utils/app_logger.dart';
 
 const _unkownErrorString = "Ocorreu um erro, tente novamente";
@@ -15,11 +16,31 @@ class ConflictException extends DioException implements Failure{
     
       @override
       // TODO: implement title
-      String get title => throw UnimplementedError();
+      String get title  {
+           if (serverResponse == null) {
+      return "Falha na Requesição";
+    }
+    return serverResponse?.data['message'] ?? "";
+  }
+      @override
+  String get message {
+    _logger.d(serverResponse?.data);
+    if (serverResponse == null) {
+      return "Erro desconhecido ";
+    } else {
+      return getErrorInfo(serverResponse?.data, errorKey);
+    }
+  }
 
     @override
     String toString(){
       return "erro em : \nTitulo $title\nMensagem: $message";
     }
+
+      String getErrorInfo(error, [String key = '']) {
+    return getErrorInfoFromResponse(error);
+  }
+
+    
     
 }
